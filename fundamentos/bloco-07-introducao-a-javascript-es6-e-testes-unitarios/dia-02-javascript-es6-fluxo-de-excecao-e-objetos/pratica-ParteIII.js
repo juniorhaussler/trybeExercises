85fdabbbf517fb6488a0813d180f4fed373ab13b
juniorhaussler <juniorhaussler@gmail.com>
2022-03-06 15:17:45 -0300

    Dia 7.2 Exercicios Bonus - 1ª questao concluida.

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/pratica-ParteIII.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/pratica-ParteIII.js
@@ -40,6 +40,86 @@ const lesson3 = {
 // const listKeys = object => Object.keys(object);
 // console.log(listKeys(lesson3));
 
-const getObjectSize = object =>  Object.keys(object).length;
+// const getObjectSize = object =>  Object.keys(object).length;
+// console.log(getObjectSize(lesson3));
 
-console.log(getObjectSize(lesson3));
+// const listKeys = object => Object.values(object);
+// console.log(listKeys(lesson3));
+
+const allLessons =  Object.assign({},{lesson1, lesson2, lesson3});
+// //console.log(allLessons);
+
+// const getNumberOfStudents = (obj) => {
+//   let total = 0;
+//   const array = Object.keys(obj);
+//   for (index in array) {
+//     total += obj[array[index]].numeroEstudantes;
+//   }
+//   return total;
+// };
+// console.log(getNumberOfStudents(allLessons));
+
+// function getValueByNumber(object, index){
+//   const array = Object.values(object);
+//   return array[index];
+// }
+
+// const getValueByNumber = (obj, index) => Object.values(obj)[index];
+
+// console.log(getValueByNumber(lesson1, 0));
+
+
+
+//link: https://www.horadecodar.com.br/2020/11/09/como-verificar-se-uma-chave-existe-em-um-objeto-javascript/
+// function verifyPair(obj,  key, value){
+  
+//   const message = (key in obj && obj[key]=== value) ? 'true' : 'false';
+//   return message;
+// }
+
+// const verifyPair = (obj, key, value) => (key in obj && obj[key]=== value) ? 'true' : 'false';
+
+// function verifyPair (obj, key, value){
+  
+//     if (obj.hasOwnProperty(key)){
+//       return true;
+//     } else {
+//         return false;
+//       }
+// }
+// console.log(verifyPair(allLessons, 'turno', 'noite'));
+
+
+
+
+//Função criada por: Breno Trindade Barbosa - turma 20 - Tribo B
+// function verifyPair(obj, key, value){
+//   const array = Object.entries(obj);
+//   for(let index in array){
+//     if(array[index][0] == key &&array[index][1] == value  ) {
+//       return true ;
+//   } else {
+//     return false;
+//     }
+//   }
+// }
+// console.log(verifyPair(lesson2,'materia','História'));
+
+
+//BÔNUS
+ // 1ª Questão
+ 
+function countStudents(obj, value){
+ const array = Object.keys(obj);
+ let sumStudent =0;
+ for (let i = 0; i < array.length; i += 1){
+  if (obj[array[i]].materia === value){
+      sumStudent += obj[array[i]].numeroEstudantes;
+  }
+ }
+ return sumStudent;
+}
+
+console.log(countStudents (allLessons, 'Matemática'));
+
+// 2ª Questão
